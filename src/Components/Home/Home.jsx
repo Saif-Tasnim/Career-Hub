1718dbbbb489f39500f7261222d49a5fb9319c77
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-05-31 19:32:19 +0600

    route setting done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import JobCategory from '../JobCategory/JobCategory';
 import FeaturedJobs from '../FeaturedJobs/FeaturedJobs';
+import Header from '../Header/Header';
 
 const Home = () => {
     return (
         <div>
+            <Header></Header>
            <Banner></Banner>
            <JobCategory></JobCategory>
            <FeaturedJobs></FeaturedJobs>
--- src/main.jsx
+++ src/main.jsx
@@ -8,16 +8,36 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Statistics from './Components/Statistics/Statistics';
+import AppliedJobs from './Components/AppliedJobs/AppliedJobs';
+import Blog from './Components/Blog/Blog';
+import JobDetail from './Components/JobDetails/JobDetail';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <Home></Home>,
+    loader: () => fetch('card_data.json') 
+  },
+  {
+    path:'/',
     element: <Layout></Layout>,
-    children : [
+    children:[
       {
-       path:'/',
-       element: <Home></Home>,
-       loader : () => fetch('card_data.json')
+        path: 'statistics',
+        element: <Statistics></Statistics>
+      },
+      {
+        path: '/appliedJobs',
+        element: <AppliedJobs></AppliedJobs>
+      },
+      {
+        path: 'blog',
+        element: <Blog></Blog>
+      },
+      {
+        path: 'job/:jobId',
+        element: <JobDetail></JobDetail>
       }
     ]
   }
