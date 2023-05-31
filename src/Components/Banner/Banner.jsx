f0361aebfe7f96836811221167a6d22789614aca
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-05-31 15:07:56 +0600

    category part start

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,26 +3,28 @@ import './Banner.css';
 
 const Banner = () => {
     return (
-        <div className='bg-[#F9F9FF] pt-11 flex justify-around items-center h-[700px]'>
-          
-           <div className="text w-[550px] h-[570px] ml-36">
-               <h1 className='text-6xl line font-bold'>One Step </h1>
-               <h1 className='text-6xl line font-bold'>Closer To Your </h1>
-               <h1 className='text-6xl line font-bold'> <span className='span'> Dream Job</span>
-               </h1>
-               <p className='mt-6'>
-               Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.
-               </p>
+        <div className='flex justify-evenly pl-28 pt-16 pb-4 bg-[#F9F9FF] '>
 
-               <button className='mt-8 custom-bg px-7 py-5 text-white text-xl font-bold'>Get Started</button>
-           </div>
-          
-           <div className="w-[816px] h-[744px] ml-24 mt-10">
-            
-            <img src="/src/images/hardy.png" alt="" />
-            
-           </div>
-        
+            <div className="text w-[550px] h-[570px]">
+                <h1 className='text-6xl line font-bold'>One Step </h1>
+                <h1 className='text-6xl line font-bold'>Closer To Your </h1>
+                <h1 className='text-6xl line font-bold'> <span className='span'> Dream Job</span>
+                </h1>
+                <p className='mt-6'>
+                    Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.
+                </p>
+
+                <button className='mt-8 custom-bg px-7 py-5 text-white text-xl font-bold'>Get Started</button>
+            </div>
+
+            <div className="w-[500px] h-[570px] ml-24 mt-10">
+
+                <img src="/src/images/hardy.png" alt="" />
+
+            </div>
+
+
+
         </div>
     );
 };
---
+++ src/Components/JobCategory/JobCategory.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './same.css';
+
+const JobCategory = () => {
+    return (
+        <div className='w-3/4 margin'>
+            i am back again
+        </div>
+    );
+};
+
+export default JobCategory;
