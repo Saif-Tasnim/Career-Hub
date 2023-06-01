fdef9dbc01b10e7d197e60b9e62f0754c3a34ff4
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-06-01 10:56:35 +0600

    applied jobs page done

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ApplyData from '../ApplyData/ApplyData';
 
 const AppliedJobs = () => {
+    const [job , setJob] = useState([]);
+
+    useEffect(()=> {
+       const dataJson = localStorage.getItem('jobs');
+       const data = JSON.parse(dataJson);
+       setJob(data);
+    } , [])
+    
     return (
-        <div>
-            Applied Jobs
+        <div className='w-3/4 mx-auto mt-32'>
+            <div className='flex justify-end'>
+               <button className='mr-10 btn-bg px-4 py-2'> Remote </button>
+                <button className='mr-10 btn-bg px-4 py-2'> Onsite </button>
+            </div>
+            
+            <div>
+
+                {
+                    job.map(j => <ApplyData
+                     key={j.id}
+                     data = {j}
+                    ></ApplyData>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/Components/ApplyData/ApplyData.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './data.css';
+
+const ApplyData = ({ data }) => {
+    console.log(data);
+
+    return (
+        <div className='flex justify-between border-2 mt-11 p-8 items-center mb-11'>
+            
+            <div className='flex gap-7 items-center'>
+                
+                {/* image div */}
+                <div>
+                    <img className='w-[240px] h-[240px]' src={data.companyLogo} alt="" />
+                </div>
+
+                {/* text div */}
+                <div>
+                    <h1 className='pt-2 font-extrabold text-xl'>{data.jobTitle}</h1>
+
+                    <p className='mb-3 mt-2'>{data.companyName}</p>
+
+                    <div className='flex gap-7 mb-5'>
+                        <div className='w-[100px] h-[35px] border-2 px-5 py-1 border-[#7E90FE] text-[#7E90FE] font-bold'>
+                            {data.remoteOrOnsite}
+                        </div>
+                        <div className='w-[120px] h-[35px] border-2 px-5 py-1 border-[#7E90FE] text-[#7E90FE] font-bold'>
+                            {data.fulltimeOrPartTime}
+                        </div>
+                    </div>
+
+                    <div className='flex gap-5 mb-10'>
+                        <div className='text-xl'>
+                            Location : {data.location}
+                        </div>
+                        <div className='text-xl'>
+                            Salary : {data.salary}
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <button className='w-[150px] h-[50px] see text-white font-bold' > View Details </button>
+        </div>
+    );
+};
+
+export default ApplyData;
